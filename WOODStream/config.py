8167a104829cdf9e49065e7cf4d9a8ce598d48bb
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-10-06 04:42:42 +0530

    Update config.py

--- WOODStream/config.py
+++ WOODStream/config.py
@@ -33,6 +33,6 @@ class Server:
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
     FQDN = str(env.get("FQDN", BIND_ADDRESS))
-    URL = "https://{}/".format(
+    URL = "https://".format(
         "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
     )
